dd5d5f7a865b75b12dae9107c33c250d8361564f
noner <pawel495@interia.pl>
2023-11-30 19:47:58 +0100

    Added some animations, checked on Iphone - App is working

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,3 +1,5 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,11 +21,6 @@ import presentation.countries.CountriesViewModel
 
 @Composable
 fun App() {
-
-    val countryApi by lazy {
-        CountryApi()
-    }
-
     MaterialTheme {
         val countryRepository = CountryRepositoryImpl(countryApi = CountryApi())
         val countriesViewModel = getViewModel(Unit,
@@ -34,15 +31,17 @@ fun App() {
             countriesViewModel.updateCountries()
         }
 
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            items(state.countries) { country ->
-                CountryListItem(
-                    countryName = country.name,
-                    flagImage = country.flag,
-                    modifier = Modifier.fillMaxWidth()
-                )
+        AnimatedVisibility(state.countries.isNotEmpty(), enter = fadeIn()) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                items(state.countries) { country ->
+                    CountryListItem(
+                        countryName = country.name,
+                        flagImage = country.flag,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
         }
     }
