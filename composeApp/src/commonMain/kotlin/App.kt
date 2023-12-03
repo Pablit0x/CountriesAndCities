bbfe6a072beede3c5542b06a93ebce2c3dbea56e
noner <pawel495@interia.pl>
2023-12-03 11:25:44 +0100

    More multiplatform way of injecting dependencies with koin

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,13 +2,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
+import di.dataModule
+import org.koin.compose.KoinApplication
 import presentation.screens.menu.ScreenMenu
 
 @Composable
 fun App() {
-    MaterialTheme {
-        Navigator(screen = ScreenMenu()) { navigator ->
-            SlideTransition(navigator = navigator)
+    KoinApplication(moduleList = {
+        listOf(dataModule)
+    }) {
+        MaterialTheme {
+            Navigator(screen = ScreenMenu()) { navigator ->
+                SlideTransition(navigator = navigator)
+            }
         }
     }
 }
